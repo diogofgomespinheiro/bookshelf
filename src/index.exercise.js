d61061a7e43e1a9ff046676e6251da436e2e4668
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-03-20 01:13:12 +0000

    finish ex01

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,28 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import * as React from 'react'
+import ReactDOM from 'react-dom'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+import {Logo} from './components/logo'
 
 // 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
 // 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+const App = () => {
+  const handleClick = alertMessage => event => {
+    console.log(event)
+    alert(alertMessage)
+  }
+  return (
+    <div>
+      <Logo height="80" width="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={handleClick('Login Clicked')}>Login</button>
+      </div>
+      <div>
+        <button onClick={handleClick('Register Clicked')}>Register</button>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
