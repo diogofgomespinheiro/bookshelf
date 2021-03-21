c2736a4ada966b2089c41c3e4cab8173b0a3be37
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-03-21 17:32:10 +0000

    finish ex03

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,11 +1,25 @@
-function client(endpoint, customConfig = {}) {
-  // 🐨 create the config you'll pass to window.fetch
-  //    make the method default to "GET"
-  // 💰 if you're confused by this, that's fine. Scroll down to the bottom
-  // and I've got some code there you can copy/paste.
-  // 🐨 call window.fetch(fullURL, config) then handle the json response
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
+async function client(endpoint, customConfig = {}) {
+  const url = `${process.env.REACT_APP_API_URL}/${endpoint}`
+  const options = {
+    method: 'GET',
+    ...customConfig,
+    headers: {
+      'Content-Type': 'application/json',
+      ...customConfig.headers,
+    },
+  }
+
+  try {
+    const response = await window.fetch(url, options)
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      return Promise.reject(data)
+    }
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 export {client}
