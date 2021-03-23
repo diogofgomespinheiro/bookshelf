73ec941b6eb5127497b0832e727ae15a4bb593a2
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-03-23 22:02:38 +0000

    finish ex05.extra-2

--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -1,6 +1,6 @@
 // export * from './authenticated-app.final'
 
-export * from './authenticated-app.exercise'
+export * from './authenticated-app.exercise.extra-2'
 
 // 💯 add `useMatch` to highlight the active nav item
 // export * from './authenticated-app.extra-2'
