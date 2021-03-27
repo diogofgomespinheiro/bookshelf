2c675be4d032c81307899c81b69fe2e8a4eb0bff
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-03-27 19:15:29 +0000

    finish ex07, extra-1, extra-2 & extra-3

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,67 @@
-// 🐨 create and export a React context variable for the AuthContext
-// 💰 using React.createContext
+import * as React from 'react'
+import * as auth from 'auth-provider'
+
+import {FullPageSpinner, FullPageErrorFallback} from 'components/lib'
+
+import {client} from 'utils/api-client'
+import {useAsync} from 'utils/hooks'
+
+const AuthContext = React.createContext()
+AuthContext.displayName = 'AuthContext'
+
+const getUser = async () => {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+const AuthProvider = ({children}) => {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
+
+  const value = {user, login, register, logout}
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+const useAuth = () => {
+  const context = React.useContext(AuthContext)
+  if (context === undefined || typeof context === 'undefined') {
+    throw new Error(`useAuth must be used within a AuthProvider`)
+  }
+  return context
+}
+
+export {AuthProvider, useAuth}
--- src/context/index.exercise.js
+++ src/context/index.exercise.js
@@ -1,2 +1,26 @@
-// this module doesn't do anything for the exercise. But you'll use this for
-// the extra credit!
+import * as React from 'react'
+import {ReactQueryConfigProvider} from 'react-query'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import {AuthProvider} from './auth-context'
+
+const queryConfig = {
+  retry(failureCount, error) {
+    if (error.status === 404) return false
+    return failureCount < 2
+  },
+  useErrorBoundary: true,
+  refetchAllOnWindowFocus: false,
+}
+
+const AppProviders = ({children}) => {
+  return (
+    <ReactQueryConfigProvider config={queryConfig}>
+      <Router>
+        <AuthProvider>{children}</AuthProvider>
+      </Router>
+    </ReactQueryConfigProvider>
+  )
+}
+
+export {AppProviders}
