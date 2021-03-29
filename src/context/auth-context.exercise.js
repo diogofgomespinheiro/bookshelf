a96cd9ebfc6af7c5bb9017a77c282ae4e15d0299
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-03-29 23:14:33 +0100

    finish ex10 and ex10.extra-1

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -3,6 +3,7 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {queryCache} from 'react-query'
 import {client} from 'utils/api-client'
 import {useAsync} from 'utils/hooks'
 import {FullPageSpinner, FullPageErrorFallback} from 'components/lib'
@@ -12,12 +13,16 @@ async function getUser() {
 
   const token = await auth.getToken()
   if (token) {
-    const data = await client('me', {token})
-    user = data.user
+    const {user: fetchedUser, listItems = []} = await client('bootstrap', {
+      token,
+    })
+    user = fetchedUser
+    queryCache.setQueryData('list-items', listItems)
   }
 
   return user
 }
+const userPromise = getUser()
 
 const AuthContext = React.createContext()
 AuthContext.displayName = 'AuthContext'
@@ -36,14 +41,6 @@ function AuthProvider(props) {
   } = useAsync()
 
   React.useEffect(() => {
-    // we need to call getUser() sooner.
-    // 🐨 move the next line to just outside the AuthProvider
-    // 🦉 this means that as soon as this module is imported,
-    // it will start requesting the user's data so we don't
-    // have to wait until the app mounts before we kick off
-    // the request.
-    // We're moving from "Fetch on render" to "Render WHILE you fetch"!
-    const userPromise = getUser()
     run(userPromise)
   }, [run])
 
