90cb60454caf322d7f4eec1836856b547b0c5eb1
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-03-30 08:23:39 +0100

    finish ex11

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,7 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+describe('Misc', () => {
+  it('should format the date to the format of month/year', () => {
+    expect(formatDate(new Date('2020-07-13'))).toBe('Jul 20')
+  })
+})
